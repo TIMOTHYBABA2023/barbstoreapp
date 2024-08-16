82c2614f0dc574a75d7a5e43c596d5d702736101
Timothy <timothybaba01@gmail.com>
2024-08-16 22:10:46 +0100

    push

--- store-app-timtrix-main/src/main/java/shoeStore/shoeStore/project/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package shoeStore.shoeStore.project.repository;
-
-//import org.springframework.data.jpa.repository.JpaRepository;
-import shoeStore.shoeStore.project.models.User;
-
-//public interface UserRepository extends JpaRepository<Long, User> {
-//}
